c86240360d96390e98dbe2ec073e3f91d3eaa329
Ben Eatock <bdeatock@gmail.com>
2023-07-10 10:07:30 +0100

    Layout skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import DesignSystem from "./pages/DesignSystem";
+
+import Logo from "./assets/Logo.svg";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div className="container">
+      <header className="flex justify-center">
+        <img className="mt-8 h-6" src={Logo} alt="logo" />
+      </header>
+      <main className="mt-11 flex flex-col">
+        <div>timer mode select</div>
+        <div>main timer with countdown</div>
+        <div>settings cog</div>
+      </main>
+    </div>
   );
 }
 
