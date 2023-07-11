d647f90b48e399e5a25c62dcd9f7f05d9048b7b6
Ben Eatock <bdeatock@gmail.com>
2023-07-11 13:04:33 +0100

    Implement inc/dec buttons

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useFormContext } from "react-hook-form";
 
 import { cn } from "@/lib/utils";
 
@@ -8,12 +9,29 @@ import iconArrowDownHover from "@/assets/icon-arrow-down-hover.svg";
 import iconArrowUpHover from "@/assets/icon-arrow-up-hover.svg";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: "text" | "number";
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     const [upHovered, setUpHovered] = React.useState(false);
     const [downHovered, setDownHovered] = React.useState(false);
+
+    const { setValue } = useFormContext();
+
+    const handleIncrement = () => {
+      if (props.name && props.value) {
+        if (Number(props.value) >= 99) return;
+        setValue(props.name, Number(props.value) + 1);
+      }
+    };
+    const handleDecrement = () => {
+      if (props.name && props.value) {
+        if (Number(props.value) <= 1) return;
+        setValue(props.name, Number(props.value) - 1);
+      }
+    };
 
     return (
       <div className="relative">
@@ -29,9 +47,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         {type === "number" && (
           <div className="absolute right-1 top-0 flex h-full flex-col">
             <button
+              type="button"
               className="flex h-full w-full flex-1 items-end pl-2 pr-2"
               onMouseEnter={() => setUpHovered(true)}
               onMouseLeave={() => setUpHovered(false)}
+              onClick={handleIncrement}
             >
               <img
                 className="mb-1 pt-1.5"
@@ -40,9 +60,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
               />
             </button>
             <button
+              type="button"
               className="flex h-full w-full flex-1 items-start pl-2 pr-2"
               onMouseEnter={() => setDownHovered(true)}
               onMouseLeave={() => setDownHovered(false)}
+              onClick={handleDecrement}
             >
               <img
                 className="visible mt-1 pb-1.5"
